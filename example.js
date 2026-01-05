11cecc9959987eb940bc13b960809d9bf7b5c28f
Harsha-220799 <n220799@rguktn.ac.in>
2026-01-05 18:47:01 +0530

    fuction examples added

---
+++ example.js
@@ -0,0 +1,25 @@
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+console.log(greet("mahesh babu")); // Output: Hello, Alice!
+
+/* functions practice */
+//named functions
+function greet(){
+    return "hello";
+}
+console.log(greet());
+
+//Anonymous functions
+const anonymous=function(){
+    return "hi siri";
+}
+console.log(anonymous());
+
+//Arrow functions
+const arrow=(n)=>{
+    return n*n;
+
+}
+console.log(arrow(5))
